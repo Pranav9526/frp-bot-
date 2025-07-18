c0870a213260bb58d9915592ffd0e40a15dc380f
Pranav <97151109+Pranav9526@users.noreply.github.com>
2025-07-18 09:35:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord import app_commands
 from discord import ui, Interaction, TextStyle, Embed, Color
 import os
 from keep_alive import keep_alive
-import datetime
+from datetime import datetime, timedelta
 import asyncio
 import re
 import logging
